2ae3cf43fb213e5c639be3f924ef4f405c8fa81d
hongliw <hlwang2019@gmail.com>
2020-09-10 08:53:49 +0800

    first commit

---
+++ jump7.py
@@ -0,0 +1,15 @@
+for i in range(1, 101):
+    if i % 7 == 0 or i % 10 == 7 or i // 10 == 7:
+        continue
+    else:
+        print(i)
+
+#i = 0
+#while i < 100:
+#    i += 1
+#    if i % 7 == 0 or i % 10 == 7 or i // 10 == 7:
+#        continue
+#    else:
+#        print(i)
+    
+
